c336b0dcdab47955a26463a843d4c869e421c7a6
onarm <90236903+onarm@users.noreply.github.com>
2022-10-04 11:33:38 +0800

    Add files via upload

---
+++ library_back/src/main/java/com/hrbsfdx/library_back/LibraryBackApplication.java
@@ -0,0 +1,13 @@
+package com.hrbsfdx.library_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryBackApplication.class, args);
+    }
+
+}
